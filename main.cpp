b2dd5b8a2158b887b2dadb8706ff03b96df04a6c
Nicole Taketa <90675454+nicoletaketa1@users.noreply.github.com>
2022-03-11 11:24:00 -0800

    add comments

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ int main() {
     int k;
     vector<int> s;
     int temp;
-
+    
     cout << "Enter k: \n";
     cin >> k;
 
+    // input allowed digits into a vector until a non-numeric value is entered
     cout << "Enter digits permitted: \n";
     int i = 0;
     while(cin >> temp){
         s.push_back(temp);
         i++;
     }
-    // test to see if vector is working
-    // for(int j = 0; j < i; j++){ cout << s[j]; }
-
+    
+    // call FindString from .cpp file
     Problem2 q;
     q.FindString(k, s);
 
